092994b015150d8182ee8e5e5d0b5c763621f2f6
willice93 <willice93@hotmail.com>
2021-02-10 00:51:10 +0100

    passage de variable twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,8 +12,20 @@ class HomeController extends Controller {
  * 
  */
 public function home(){
+
+
+    $name = 'willice';
+    $txt = 'Lorem ipsum dolor sit amet.'; 
     return $this->render(
-        'home.html.twig'
+        'home.html.twig',
+        [
+            // this array defines the variables passed to the template,
+            // where the key is the variable name and the value is the variable value
+            // (Twig recommends using snake_case variable names: 'foo_bar' instead of 'fooBar')
+            'name' => $name,
+        'txt' => $txt,
+        ]
+    
     );
 }
 }

19b90a88505ace0ed6ee51155bcc1a475626f8c8
sebi fad <sebi.fadis@gmail.com>
2021-04-22 16:43:56 +0200

    identification

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,11 +1,11 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class HomeController extends Controller {
+class HomeController extends AbstractController {
 
 /**
  * @Route("/", name="homepage")

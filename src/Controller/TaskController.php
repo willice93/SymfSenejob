85e59bc97e743676fbf965a74c5efce5ec9f908d
willice93 <willice93@hotmail.com>
2021-05-16 05:55:20 +0200

    creation du formulaire create tache

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Task;
+use App\Form\TaskType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TaskController extends AbstractController
 {
@@ -16,5 +18,21 @@ class TaskController extends AbstractController
         return $this->render('task/index.html.twig', [
             'controller_name' => 'TaskController',
         ]);
+    }
+
+    /**
+     * @Route("/create", name="createtask")
+     */
+    public function createTask(){
+ $task = new Task;
+
+ $form = $this->createForm(TaskType::class, $task);
+        return $this->render('new.html.twig', [
+            
+            'form' => $form->createView(),
+]);
+
+
+
     }
 }
--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -204,6 +204,11 @@ class Client
 
         return $this;
     }
-
+public function __toString(){
+        // to show the name of the Category in the select
+        return $this->lastNameClient;
+        // to show the id of the Category in the select
+        // return $this->id;
+    }
     
 }

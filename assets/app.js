1066ccb642f01a7d38673fcd042bb95f71913d5b
willice93 <willice93@hotmail.com>
2021-06-22 18:48:50 +0200

    react et api

---
+++ assets/app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom';
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+// start the Stimulus application
+import './bootstrap';
+
+//javascript
+console.log("hello word !!!!");
+
+const App =() =>{
+    return <h1>Bonjour</h1>;
+};
+const rootElement = document.querySelector('#app');
+ReactDOM.render(<App />, rootElement);
